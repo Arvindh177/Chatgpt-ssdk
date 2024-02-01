04e486293ca766482e0df6d770e7c88f9cb04213
Arvindh177 <arvindhdevlps17@gmail.com>
2024-02-01 23:26:28 +0530

    Initial commit

---
+++ src/components/constants/constants.js
@@ -0,0 +1,4 @@
+export const chatGptToken = "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";
+export const chatGptUrl = "https://api.openai.com/v1/chat/completions";
+export const url = "https://api.surveysparrow.com";
+export const seperator = ":-:";
